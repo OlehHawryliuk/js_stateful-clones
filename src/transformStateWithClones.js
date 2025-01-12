a1cf8b045ca58a1a0e16bf5d143a39a6418525f8
Oleh Havrylyuk <edifier373@gmail.com>
2025-01-12 17:11:05 +0200

    final Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -15,25 +15,21 @@ function transformStateWithClones(state, actions) {
       for (const key in i.extraData) {
         newState[key] = i.extraData[key];
       }
-
-      stateArrey.push({ ...newState });
     }
 
     if (i.type === 'removeProperties') {
       for (const key of i.keysToRemove) {
         delete newState[key];
       }
-
-      stateArrey.push({ ...newState });
     }
 
     if (i.type === 'clear') {
       for (const key in newState) {
         delete newState[key];
       }
-
-      stateArrey.push({ ...newState });
     }
+
+    stateArrey.push({ ...newState });
   }
 
   return stateArrey;
